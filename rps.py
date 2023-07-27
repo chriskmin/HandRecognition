486601651b52d67960a9be97be59291c450e316c
Chris Min <chrismin@Bu.edu>
2023-07-27 15:37:41 -0400

    Implemented rps function

--- rps.py
+++ rps.py
@@ -4,6 +4,15 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
+def getHandMove(hand_landmarks):
+    landmarks = hand_landmarks.landmark
+    if all([landmarks[i].y < landmarks[i+3].y for i in range (9,20,4)]):
+        return "rock"
+    elif landmarks[13].y < landmarks[16].y and landmarks[17].y < landmarks[20].y:
+        return "scissors"
+    else:
+        return "paper"
+
 vid = cv.VideoCapture(0)
 
 with mp_hands.Hands(
@@ -12,11 +21,27 @@ with mp_hands.Hands(
     min_tracking_confidence=0.5) as hands:
     
     while True:
-        ret, frame = vid.read()
-        if not ret or frame is None:
+        ret, image = vid.read()
+        if not ret or image is None:
             break
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
         
-        cv.imshow('frame', frame)
+        results = hands.process(image)
+        
+        image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+                
+        image = cv.flip(image, 1)
+        
+        
+        cv.imshow('image', image)
         
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
