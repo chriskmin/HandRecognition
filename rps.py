7454beb8c84d0a819ac8c7dd2ae07bb802c1d289
Chris Min <chrismin@Bu.edu>
2023-07-27 17:35:14 -0400

    Implemented rps game on opencv player, going to implement onto a cleaner gui

--- rps.py
+++ rps.py
@@ -12,6 +12,14 @@ def getHandMove(hand_landmarks):
         return "scissors"
     else:
         return "paper"
+    
+    
+clock = 0 
+p1_move = p2_move = None
+gameText = ""
+success = True
+P1score = 0
+P2score = 0
 
 vid = cv.VideoCapture(0)
 
@@ -40,6 +48,47 @@ with mp_hands.Hands(
                 
         image = cv.flip(image, 1)
         
+        
+        if 0 <= clock < 20:
+            success = True
+            gameText= "Ready?"
+            
+        elif clock < 30:
+            gameText="rock, ..."
+        elif clock < 40:
+            gameText="paper, ..."
+        elif clock < 50:
+            gameText="scissors, ..."
+        elif clock < 60:
+            gameText="SHOOT!"
+        elif clock == 60:
+            hls = results.multi_hand_landmarks 
+            if hls and len(hls) == 2:
+                p1_move = getHandMove(hls[0])
+                p2_move = getHandMove(hls[1])
+            else:
+                success = False
+        
+        
+        elif clock < 100:
+            if success:
+                gameText = f"Played 1 played {p1_move}. Player 2 played {p2_move}."
+                if p1_move == p2_move: gameText = f"{gameText} Tie!"
+                elif (p1_move == "paper" and p2_move == "rock"):
+                    gameText = f"{gameText} Player 1 wins!"
+                elif (p1_move == "scissors" and p2_move == "paper"):
+                    gameText = f"{gameText} Player 1 wins!"
+                elif (p1_move == "rock" and p2_move == "scissiors"):
+                    gameText = f"{gameText} Player 1 wins!"
+                else:
+                    gameText= f"{gameText} Player 2 wins!"
+            else:
+                gameText = "Didn't play"
+            
+        
+        cv.putText(image, f"Clock: {clock}", (50,50), cv.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2, cv.LINE_AA)
+        cv.putText(image, gameText, (50,80), cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2, cv.LINE_AA)
+        clock = (clock + 1) % 100
         
         cv.imshow('image', image)
         
